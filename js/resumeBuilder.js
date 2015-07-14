dab20aa25cf5831a2d1bcbaba73b9c17cfc298d2
Jason Lam <JasonLam@jasons-mbp.attlocal.net>
2015-07-13 22:21:26 -0700

    change 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,26 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-$("#main").append("Jason Lam");
+/*
+var awesomeThoughts = "I am Jason and I am AWESOME";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN")
+*/
+
+/*
+var name = "Jason Lam";
+var role = "Web developer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+*/
+
+var bio = {
+	"name": "Jason",
+	"role": "Web developer",
+	"pictureURL": "https://commons.wikimedia.org/wiki/File:Googlelogo.png",
+	"welcomeMessage": "Hi, Welcome!",
+	"skills": ["programming", "swimming", "singing"]
+};
+
+$("#main").append(bio.name);
